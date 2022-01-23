4c9f90bb8a1624f6b89a81464fc4195ee07be275
Vitalii Skachkov <vitalii.skachkov@asml.com>
2022-01-23 22:26:36 +0100

    Temporary commit

--- src/App.js
+++ src/App.js
@@ -1,152 +1,211 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component, useState } from "react";
+import React, {Component, useState} from "react";
 import moment from "moment";
 import {
-  Button,
-  ButtonDropdown,
-  ButtonGroup,
-  Card, CardBody,
-  CardHeader,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle
+    Button,
+    ButtonDropdown,
+    ButtonGroup,
+    Card, CardBody,
+    CardHeader,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle
 } from "reactstrap";
 import Timeline from "react-visjs-timeline";
 import NewAction from "./NewAction";
+
 const groups = [
-  {
-    id: "a1",
-    content: "Study"
-    /*     subgroupOrder: (a, b) => {
-      console.log("What are we doing here??");
-      console.log("A:", a, "B:", b);
-      let r = 0;
-      if (a.subgroup === 2) return (r = 1);
-      if (b.subgroup === 5) return (r = -1);
-      console.log("Result:", r);
-      return r;
-    } */
-  },
-  {
-    id: "a2",
-    content: "Work",
-    subgroupOrder: "sborder"
-  },
-  {
-    id: "a3",
-    content: "Life",
-    subgroupOrder: "sborder"
-  }
-];
-const options = {
-  editable: {
-    add: true,
-    remove: false,
-    updateGroup: false,
-    updateTime: true
-  },
-  horizontalScroll: true,
-  margin: {
-    axis: 5,
-    item: {
-      vertical: 5,
-      horizontal: 0
+    {
+        id: "a1",
+        content: "Study"
+        /*     subgroupOrder: (a, b) => {
+          console.log("What are we doing here??");
+          console.log("A:", a, "B:", b);
+          let r = 0;
+          if (a.subgroup === 2) return (r = 1);
+          if (b.subgroup === 5) return (r = -1);
+          console.log("Result:", r);
+          return r;
+        } */
+    },
+    {
+        id: "a2",
+        content: "Work",
+        subgroupOrder: "sborder"
+    },
+    {
+        id: "a3",
+        content: "Life",
+        subgroupOrder: "sborder"
     }
-  },
-  moveable: true,
-  orientation: {
-    axis: "both",
-    item: "top"
-  },
-  start: moment()
-      .subtract(4, "days")
-      .format(),
-  end: moment()
-      .add(4, "weeks")
-      .format(),
-  stack: true,
-  stackSubgroups: false,
-  type: "range",
-  width: "100%",
-  zoomable: true,
-  zoomKey: 'ctrlKey',
-  zoomMin: 147600000,
-  zoomMax: 51840000000
+];
+const INITIAL_OPTIONS = {
+    editable: {
+        add: true,
+        remove: false,
+        updateGroup: false,
+        updateTime: true
+    },
+    horizontalScroll: true,
+    margin: {
+        axis: 5,
+        item: {
+            vertical: 5,
+            horizontal: 0
+        }
+    },
+    moveable: true,
+    orientation: {
+        axis: "both",
+        item: "top"
+    },
+    start: "2018-01-01",
+    end: "2018-01-05",
+    stack: true,
+    stackSubgroups: false,
+    type: "range",
+    width: "100%",
+    zoomable: true,
+    zoomKey: 'ctrlKey',
+    zoomMin: 600000, // one minute
+    zoomMax: 51840000000
 };
 const INITIAL_ITEMS = [
-  {
-    start: moment()
-        .subtract(7, "days")
-        .format(),
-    end: moment()
-        .subtract(1, "days")
-        .format(), // end is optional
-    content: "<img src=\"https://www.w3schools.com/images/w3schools_green.jpg\" alt=\"W3Schools.com\" style=\"width:128px;height:128px;\"><p>Hello World</p>",
-    group: "a1"
-  },
-  {
-    start: moment()
-        .subtract(4, "days")
-        .format(),
-    end: moment()
-        .subtract(2, "days")
-        .format(), // end is optional
-    content: "Hi what's up",
-    group: "a1"
-  },
-  {
-    start: moment()
-        .subtract(5, "days")
-        .format(),
-    end: moment().format(), // end is optional
-    content: "Step2",
-    group: "a2"
-  }
+    {
+        start: "2018-01-02",
+        end: "2018-01-03", // end is optional
+        content: "<img src=\"https://www.w3schools.com/images/w3schools_green.jpg\" style=\"width:128px;height:128px;\"><p>Hello World</p>",
+        group: "a1"
+    },
+    {
+        start: "2018-01-02",
+        end: "2018-01-03", // end is optional
+        content: "<p>Hi what's up</p> " +
+            "<p>How is it</p>" +
+            "<p>WoW</p>" +
+            "<p>Asadasd</p>" +
+            "<p>asdasdasd</p>",
+        group: "a1"
+    },
+    {
+        start: moment()
+            .subtract(5, "days")
+            .format(),
+        end: moment().format(), // end is optional
+        content: "Step2",
+        group: "a2"
+    }
 ];
 const DisplayComponent = () => {
     const [items, setActions] = useState(INITIAL_ITEMS);
+    const [options, setOptions] = useState(INITIAL_OPTIONS);
     let actionButtons;
     actionButtons = (
         <div className="card-header-actions">
 
-          <ButtonGroup className="float-right">
-            <ButtonDropdown isOpen={true}>
-              <DropdownToggle className="p-0 btn btn-setting" color="transparent">
-                <i
-                    className="card-header-action btn btn-setting icon-settings"
-                    title="Timeline actions"
-                />
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem disabled>{"Combine Systems"}</DropdownItem>
-                <DropdownItem>Generate New PM's</DropdownItem>
-                <DropdownItem>{"Show Action Plans"}</DropdownItem>
-              </DropdownMenu>
-            </ButtonDropdown>
-          </ButtonGroup>
+            <ButtonGroup className="float-right">
+                <ButtonDropdown isOpen={true}>
+                    <DropdownToggle className="p-0 btn btn-setting" color="transparent">
+                        <i
+                            className="card-header-action btn btn-setting icon-settings"
+                            title="Timeline actions"
+                        />
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem disabled>{"Combine Systems"}</DropdownItem>
+                        <DropdownItem>Generate New PM's</DropdownItem>
+                        <DropdownItem>{"Show Action Plans"}</DropdownItem>
+                    </DropdownMenu>
+                </ButtonDropdown>
+            </ButtonGroup>
         </div>
     );
 
+    const rangeChangeHandler = (change) => {
+        const new_options = {
+            ...INITIAL_OPTIONS,
+            start: change.start,
+            end: change.end,
+        };
+
+        const request = {
+            start: change.start,
+            end: change.end
+            // start: "2018-01-02",
+            // end: "2018-01-03"
+        };
+        fetch("http://localhost:8080/get/messages/", {
+            method: 'POST',
+            body: JSON.stringify(request),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            const transformedItems = data.map(item => {
+                return {
+                    start: item.msgDate,
+                    end: moment(item.msgDate).add(50000000),
+                    content: item.text,
+                    group: 'a1'
+                }
+            });
+            setActions(transformedItems);
+        });
+        // setActions([item]);
+        setOptions(new_options);
+    }
+
     const addActionHandler = (action) => {
-      console.log("in saveNewActionHandler in App")
-      console.log(action);
-      setActions((prevActions)  => {
-        return [action, ...prevActions];
+      const request = {
+        // start: change.start,
+        // end: change.end
+        start: "2018-01-02",
+        end: "2018-01-03"
+      };
+      fetch("http://localhost:8080/get/messages/", {
+        method: 'POST',
+        body: JSON.stringify(request),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => {
+        return response.json();
+      }).then(data => {
+          console.log(data);
+        const transformedItems = data.map(item => {
+          return {
+            start: item.msgDate,
+            end: "2018-01-03",
+            content: item.authorName,
+            group: 'a1'
+          }
+        });
+        setActions(transformedItems);
       });
+      // const items = [ {
+      //   start: '2018-01-02',
+      //   end: '2018-01-03',
+      //   content: 'Hello',
+      //   group: "a3"
+      // }]
+      // setActions(items);
     };
+
     return (
         <div>
-          <Card>
-            <CardHeader>
-              <strong>Timeline</strong>
-              {actionButtons}
-            </CardHeader>
-            <NewAction onAddNewAction={addActionHandler}></NewAction>
-            <CardBody>
-              <Timeline options={options} items={items} groups={groups} />
-            </CardBody>{" "}
-          </Card>
+            <Card>
+                <CardHeader>
+                    <strong>Timeline</strong>
+                    {actionButtons}
+                </CardHeader>
+                <NewAction onAddNewAction={addActionHandler}></NewAction>
+                <CardBody>
+                    <Timeline options={options} items={items} groups={groups} rangechangeHandler={rangeChangeHandler}/>
+                </CardBody>{" "}
+            </Card>
         </div>
     );
 }
